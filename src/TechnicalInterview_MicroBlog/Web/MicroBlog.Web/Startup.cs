6e87a74e8e1673013af09ca604869cf18e52aad3
Jeremy Abbott <jeremymabbott@gmail.com>
2014-12-18 13:03:34 -0600

    Added solution and updated readme.

---
+++ src/TechnicalInterview_MicroBlog/Web/MicroBlog.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MicroBlog.Web.Startup))]
+namespace MicroBlog.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
